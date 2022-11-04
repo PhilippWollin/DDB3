7e065cf9ff4bf423c7ce6adea0b366adaa471986
Philipp Wollin <philipp.wollin@medsolv.com>
2022-11-04 13:28:15 +0100

    fist look new DDB

---
+++ src/components/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import rocket from "../Img/rocket.webp"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img className="card--image" src={rocket} alt="a rocket lifting off"></img>
+            <span className="card--name">Project name</span>
+            <span className="card--result">75% done</span>
+        </div>
+    )
+};
---
+++ src/components/date.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Date() {
+    return (
+        <div className="card">
+            <span className="card--result">75% done</span>
+        </div>
+    )
+};
